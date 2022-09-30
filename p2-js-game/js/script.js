2203ea163b476f651ea7dfd03813764d5c8eecb0
jrosi <j.rosimo098@gmail.com>
2022-09-30 11:17:09 +0800

    Feat: modified map create function and added changing direction function

--- p2-js-game/js/script.js
+++ p2-js-game/js/script.js
@@ -14,31 +14,66 @@ let foodYCoor;
 
 //variables of the snake head / start box
 let snakeXCoor = sizeOfBlock * 5;
-let snakeYCoor;
+let snakeYCoor = sizeOfBlock * 5;
 
+//coordinate for moving the snake
+let moveXCoor = 0;
+let moveYCoor = 0;
+
+let snakeFrame = [];
+
+let gameLost = false;
 
 createMap(); //call map
 
 function createMap() {
 
-    context.fillStyle = 'black';
+    context.fillStyle = 'rgb(0,0,0, 0.3)';
     context.fillRect(0, 0, map.width, map.height);
 
     context.fillStyle = 'red';
     context.fillRect(0, 0, foodXCoor, foodYCoor);
 
-    if(snakeXCoor == foodXCoor && snakeYCoor == foodYCoor) {
+    if (snakeXCoor == foodXCoor && snakeYCoor == foodYCoor) {
+        snakeFrame.push([foodXCoor, foodYCoor]);
+    }
+    for ( let count = snakeFrame.length - 1; count > 0; count--) {
+        snakeFrame[count] = snakeFrame[count - 1];
+    }
+    if (snakeFrame.length) {
+        snakeFrame[0] = [snakeXCoor, snakeYCoor];
+    }
 
+    context.fillStyle = 'green';
+    snakeXCoor = moveXCoor * sizeOfBlock;
+    snakeYCoor = moveYCoor * sizeOfBlock;
+    context.fillRect(snakeXCoor, snakeYCoor, sizeOfBlock, sizeOfBlock);
+    for (let count = 0; i < snakeFrame.length; i++) {
+        context.fillRect(snakeFrame[count][0], snakeFrame[count][1], sizeOfBlock, sizeOfBlock);
+    }
+
+    //detection for collision
+    if (snakeXCoor < 0 || snakeXCoor > col * sizeOfBlock || snakeYCoor < 0 || snakeYCoor > col * sizeOfBlock) {
+    gameLost = true;
+    alert('You Died!');
+    }
+
+    for (let count = 0; i < snakeFrame.length; count++) {
+        if (snakeXCoor == snakeFrame[count][0] && snakeYCoor == snakeFrame[count][1]) {
+            gameLost = true;
+            alert('You Died!');
+        }
     }
 }
 
+function changePath(event) {
+    if(event.code == 'ArrowLeft' && moveYCoor != 1) {
+        moveXCoor = -1;
+        moveYCoor = 0;
+    }
 
-
-
-
-
-
-function createMap() {
-    context.fillStyle = 'black';
-    context.fillRect(0, 0, map.width, map.height)
+    if(event.code == 'ArrowRight' && moveXCoor != -1) {
+        
+    }
 }
+
